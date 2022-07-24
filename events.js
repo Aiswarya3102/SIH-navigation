19a57074daac7d5843b1d539d12ba0026fab395b
Aiswarya3102 <92567537+Aiswarya3102@users.noreply.github.com>
2022-07-24 15:21:07 +0530

    Add files via upload

---
+++ events.js
@@ -0,0 +1,38 @@
+import React from 'react';
+/*import "bootstrap/dist/css/bootstrap.min.css";*/
+/*import {Carousel} from "react-bootstrap";*/
+
+function events() {
+return (
+	/*<Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src="https://www.istockphoto.com/photos/nature"
+          height="500"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src="https://www.iccr.gov.in/sites/default/files/POS%20-%20Odissi%20-%2005_0.jpg"
+          height="500"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.ytimg.com/vi/SiW8P42C0Ck/maxresdefault.jpg"
+          height="500"
+          alt="Third slide"
+        />
+        
+      </Carousel.Item>
+    </Carousel>*/
+	<h1>this is event page</h1>
+);
+};
+
+export default events;

4fff3c2067d20a3e4f459c979f0f4ffa24a5aca0
Aiswarya3102 <92567537+Aiswarya3102@users.noreply.github.com>
2022-07-24 15:22:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import React from "react";
+import { Nav, NavLink, NavMenu } 
+    from "./NavbarElements";
+  
+    /*const Navbar = () => {
+      return (
+        <div className="navBar">
+            <ul className="navBarList">
+               <Link to="/home" ><li className="navList">Home</li></Link>
+               <Link to="/about" ><li className="navList">About</li></Link>
+               <Link to="/contact" ><li className="navList">Contact</li></Link>
+               <Link to="/events" ><li className="navList">Events</li></Link>
+               <Link to="/login" ><li className="navList">Login</li></Link>
+            </ul>
+        </div>
+      );
+    };*/
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+        <img
+              src='https://www.creditboss.in/wp-content/uploads/2020/10/Process-of-funding.jpg'
+              height='30'
+              alt=''
+              loading='lazy'
+            />
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/events" activeStyle>
+            Events
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up/Sing In
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
-reportWebVitals();
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-
-	<App />,
-document.getElementById('root')
-);*/
 
